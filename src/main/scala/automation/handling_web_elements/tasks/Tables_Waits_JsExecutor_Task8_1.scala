a7be33e2b520795d72e0012a902b8a224fcb0e7d
Pablo <pablo.montalvo@mercator.group>
2025-06-13 12:52:33 +0100

    add for refactor of assert test of language found

--- src/main/scala/automation/handling_web_elements/tasks/Tables_Waits_JsExecutor_Task8_1.scala
+++ src/main/scala/automation/handling_web_elements/tasks/Tables_Waits_JsExecutor_Task8_1.scala
@@ -5,7 +5,6 @@ import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.support.ui.{ExpectedConditions, FluentWait, WebDriverWait}
 
 import java.time.Duration
-import java.util.function.Function
 import scala.jdk.CollectionConverters._
 
 object Tables_Waits_JsExecutor_Task8_1 extends App {
@@ -23,7 +22,7 @@ object Tables_Waits_JsExecutor_Task8_1 extends App {
   // Get all rows inside tbody
   val rows: Iterable[WebElement] = countriesTable.findElements(By.cssSelector("tbody > tr")).asScala
   println("List of countries:")
- // Loop through all the rows except the first one
+  // Loop through all the rows except the first one
   for (row <- rows.drop(1)) {
     // Highlight row with yellow background as it's read
     jsExecutor.executeScript("arguments[0].style.backgroundColor = 'yellow'", row)
@@ -66,7 +65,7 @@ object Tables_Waits_JsExecutor_Task8_1 extends App {
   // 4. Wait until a specific row is visible
   val fluentWait = new FluentWait[WebDriver](driver)
     .withTimeout(Duration.ofSeconds(15)) // max wait time
-    .pollingEvery(Duration.ofMillis(500))  // polling interval
+    .pollingEvery(Duration.ofMillis(500)) // polling interval
     .ignoring(classOf[org.openqa.selenium.NoSuchElementException]) // ignore if not found immediately
 
   fluentWait.until((d: WebDriver) => {
@@ -81,16 +80,15 @@ object Tables_Waits_JsExecutor_Task8_1 extends App {
 
   // 5. Assertion of row found
   var found = false
-  rows.foreach {row =>
-    val cells = row.findElements(By.tagName("td"))
-    val poland : WebElement = cells.get(1)
-    if (poland.getText == "Poland") {
-      assert(cells.get(4).getText == "Polish")
+  rows.foreach { row =>
+    val cells = row.findElements(By.tagName("td")).asScala
+    val language: WebElement = cells(4)
+    if (language.getText == "Polish") {
       found = true
       jsExecutor.executeScript("arguments[0].style.backgroundColor = 'blue'", row)
-      }
     }
+  }
+  assert(found, "No row with Polish language has been found")
   println(s"Polish found? $found")
-
 
 }
